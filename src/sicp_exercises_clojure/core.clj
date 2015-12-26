89b7a0f05643a6e734a09e17cd6e9fbcfb9882f5
Daniel Ramirez <javamonn@gmail.com>
2015-12-26 16:24:48 -0500

    exercise 1-11

--- src/sicp_exercises_clojure/core.clj
+++ src/sicp_exercises_clojure/core.clj
@@ -1,6 +1,28 @@
 (ns sicp-exercises-clojure.core)
 
+(defn exercise-1-11
+  [n]
+  (defn rec
+    [n]
+    (cond
+      (< n 3) n
+      (>= n 3) (+ 
+                 (rec (dec n))
+                 (* (rec (- n 2)) 2)
+                 (* (rec (- n 3)) 3))))
+  (defn iter
+    [a b c counter]
+    (if
+      (< counter 3) 
+        a
+        (iter (+ a (* b 2) (* c 3))
+              a
+              b
+              (- counter 1))))
+
+  (println (rec n))
+  (println (iter 2 1 0 n)))
+
 (defn main
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [n]
+  (exercise-1-11 (Integer/parseInt n)))
