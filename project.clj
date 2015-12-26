4a44f3343558471f8d37ecd0f9da43332e372eda
Daniel Ramirez <javamonn@gmail.com>
2015-12-26 14:07:38 -0500

    init lein project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sicp-exercises-clojure "0.1.0-SNAPSHOT"
+  :description "SICP exercises implemented in Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main sicp-exercises-clojure.core/main)
